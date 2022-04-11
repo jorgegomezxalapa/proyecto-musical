1b2b6fd383213736730e1b1d5dbc9f5f3b280d6b
Ivan Ramos <jrgz305@gmail.com>
2022-04-11 14:51:39 -0500

    Metodo show, showById,store,update de AlbumController

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Response;
+use App\Models\Album;
+use Illuminate\Support\Facades\DB;
 
 class AlbumController extends Controller
 {
+    protected $albumes;
+    public function __construct(Album $albumes)
+    {
+        $this->albumes = $albumes;
+    }
    
     /**
     *Regresa el listado completo de la tabla Album 
@@ -14,7 +20,11 @@ class AlbumController extends Controller
     */
     public function show()
     {
-        //
+        try {
+            $albumes = $this->albumes->obtenerAlbumes();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -24,7 +34,11 @@ class AlbumController extends Controller
     */
     public function showById($id)
     {
-        //
+        try {
+            $album = $this->albumes->obtenerAlbumPorId($id);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -41,6 +55,16 @@ class AlbumController extends Controller
             ],404);
         }else{
             //to do
+            $validated = $request->validate([
+                'nombre' => 'required|max:255',
+                'fecha_publicacion' => 'required|date',
+                'genero' => 'required|max:255'
+            ]);
+
+            DB::beginTransaction();
+            $album = new Album($request->all());
+            $album->save();
+            DB::commit();
 
         }
     }
@@ -60,6 +84,18 @@ class AlbumController extends Controller
             ],404);
         }else{
             //to do
+            $validated = $request->validate([
+                'id' => 'required',
+                'nombre' => 'required|max:255',
+                'fecha_publicacion' => 'required|date',
+                'genero' => 'required\max:255'
+            ]);
+
+            DB::beginTransaction();
+            $album = Album::find($request->id);
+            $album->fill($request->all());
+            $album->save();
+            DB::commit();
 
         }
     }
