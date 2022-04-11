dbac79edd3bf50bd33b2cc487ecdcd098868d915
Ivan Ramos <jrgz305@gmail.com>
2022-04-11 13:23:52 -0500

    funciones sin metodos de los controladores artistas, albumes, y canciones

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -3,8 +3,74 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Response;
 
 class AlbumController extends Controller
 {
-    //
+   
+    /**
+    *Regresa el listado completo de la tabla Album 
+    * @return \Illuminate\Http\Response
+    */
+    public function show()
+    {
+        //
+    }
+
+    /**
+    *Regresa la fila del id de la tabla Album
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function showById($id)
+    {
+        //
+    }
+
+    /**
+    *Almacena una nueva fila en la tabla Album
+    * @param \Illuminate\Http\Request $request
+    * @return \Illuminate\Http\Response
+    */
+    public function store(Request $request)
+    {
+        if(! $request->ajax()){
+            return response()->json([
+                'mensaje' => 'Error',
+                'codigo' => 1,
+            ],404);
+        }else{
+            //to do
+
+        }
+    }
+
+
+    /**
+    *Actualiza el registro en la tabla Album
+    * @param \Illuminate\Http\Request $request
+    * @return \Illuminate\Http\Response
+    */
+    public function update(Request $request)
+    {
+        if(! $request->ajax()){
+            return response()->json([
+                'mensaje' => 'Error',
+                'codigo' => 1,
+            ],404);
+        }else{
+            //to do
+
+        }
+    }
+
+    /**
+    * Elimina el registro de la tabla Album
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function delete($id)
+    {
+        //
+    }
 }
--- app/Http/Controllers/ArtistaController.php
+++ app/Http/Controllers/ArtistaController.php
@@ -6,5 +6,69 @@ use Illuminate\Http\Request;
 
 class ArtistaController extends Controller
 {
-    //
+    /**
+    *Regresa el listado completo de la tabla Album 
+    * @return \Illuminate\Http\Response
+    */
+    public function show()
+    {
+        //
+    }
+
+    /**
+    *Regresa la fila del id de la tabla Album
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function showById($id)
+    {
+        //
+    }
+
+    /**
+    *Almacena una nueva fila en la tabla Album
+    * @param \Illuminate\Http\Request $request
+    * @return \Illuminate\Http\Response
+    */
+    public function store(Request $request)
+    {
+        if(! $request->ajax()){
+            return response()->json([
+                'mensaje' => 'Error',
+                'codigo' => 1,
+            ],404);
+        }else{
+            //to do
+
+        }
+    }
+
+
+    /**
+    *Actualiza el registro en la tabla Album
+    * @param \Illuminate\Http\Request $request
+    * @return \Illuminate\Http\Response
+    */
+    public function update(Request $request)
+    {
+        if(! $request->ajax()){
+            return response()->json([
+                'mensaje' => 'Error',
+                'codigo' => 1,
+            ],404);
+        }else{
+            //to do
+
+        }
+    }
+
+    /**
+    * Elimina el registro de la tabla Album
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function delete($id)
+    {
+        //
+    }
 }
