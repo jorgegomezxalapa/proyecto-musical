6ad14be8b02314320927a8b201c6f605deaedea0
Ivan Ramos <jrgz305@gmail.com>
2022-04-11 14:52:01 -0500

    Metodo show, showById,store,update de ArtistaController

--- app/Http/Controllers/ArtistaController.php
+++ app/Http/Controllers/ArtistaController.php
@@ -3,16 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Artista;
+use Illuminate\Support\Facades\DB;
 
 class ArtistaController extends Controller
 {
-    /**
+    protected $artistas;
+    public function __construct(Artista $artistas)
+    {
+        $this->artistas = $artistas;
+    }
+        /**
     *Regresa el listado completo de la tabla Album 
     * @return \Illuminate\Http\Response
     */
     public function show()
     {
-        //
+        try {
+            $artistas = $this->artistas->obtenerArtistas();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+        
     }
 
     /**
@@ -22,7 +34,11 @@ class ArtistaController extends Controller
     */
     public function showById($id)
     {
-        //
+        try {
+            $artista = $this->artistas->obtenerArtistaPorId($id);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -32,14 +48,30 @@ class ArtistaController extends Controller
     */
     public function store(Request $request)
     {
-        if(! $request->ajax()){
-            return response()->json([
-                'mensaje' => 'Error',
-                'codigo' => 1,
-            ],404);
-        }else{
-            //to do
-
+        try {
+            if(! $request->ajax()){
+                return response()->json([
+                    'mensaje' => 'Error',
+                    'codigo' => 1,
+                ],404);
+            }else{
+                //to do
+                $validated = $request->validate([
+                    'nombre' => 'required|max:255',
+                    'fecha_formacion' => 'required|date',
+                    'genero' => 'required\max:255',
+                    'origen' => 'required|max:255',
+                ]);
+                DB::beginTransaction();
+                $artista = new Artista($request->all());
+                $artista->save();
+                DB::commit();
+                //return
+    
+            }
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            //throw $th;
         }
     }
 
@@ -51,14 +83,32 @@ class ArtistaController extends Controller
     */
     public function update(Request $request)
     {
-        if(! $request->ajax()){
-            return response()->json([
-                'mensaje' => 'Error',
-                'codigo' => 1,
-            ],404);
-        }else{
-            //to do
-
+        try {
+            if(! $request->ajax()){
+                return response()->json([
+                    'mensaje' => 'Error',
+                    'codigo' => 1,
+                ],404);
+            }else{
+                //to do
+                $validated = $request->validate([
+                    'id' => 'required',
+                    'nombre' => 'required|max:255',
+                    'fecha_formacion' => 'required|date',
+                    'genero' => 'required|max:255',
+                    'origen' => 'required|max:255',
+                ]);
+                DB::beginTransaction();
+                $artista = Artista::find($request->id);
+                $artista->fill($request->all());
+                $artista->save();
+                DB::commit();
+    
+    
+            }
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            //throw $th;
         }
     }
 
